8d8f774bacf47d6466e320d2c8c71fc2fb375fff
thellllima02 <thelllima02@hotmail.com>
2023-04-08 11:39:37 -0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('olá mundo');
+import fs from 'fs';
+import chalk from 'chalk';
+
+function pegaArquivo(caminhoDoArquivo){
+   const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (_, texto)=>{
+        console.log(chalk.green(texto));
+    } )
+}
+
+pegaArquivo('./arquivos/texto.md')
