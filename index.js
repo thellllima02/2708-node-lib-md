666a5e1ba56072ccc6c9aede49fc58c55586d6b7
thellllima02 <thelllima02@hotmail.com>
2023-06-29 21:07:06 -0300

    .

--- index.js
+++ index.js
@@ -10,15 +10,33 @@ function trataErro(erro){
     //lançar,mostrar           todo erro tem um código
     throw new Error(chalk.red(erro.code , 'não há arquivo no diretório'));
 }
+// async/await
 
-//Código assíngrono tipo 1
-function pegaArquivo(caminhoDoArquivo){
+async function pegaArquivo(caminhoDoArquivo){
+   try{
     const encoding = 'utf-8';
-    fs.promises
-     .readFile(caminhoDoArquivo, encoding )
-     .then((texto)=>console.log(chalk.green(texto)))
-     .catch(trataErro)
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto));
+   }catch(erro){
+    trataErro(erro)
+   }
 }
+
+
+
+
+
+
+
+
+//Código assíngrono tipo then()
+//function pegaArquivo(caminhoDoArquivo){
+//    const encoding = 'utf-8';
+//    fs.promises
+//     .readFile(caminhoDoArquivo, encoding )
+//     .then((texto)=>console.log(chalk.green(texto)))
+//     .catch(trataErro)
+//}
 
 //Código síncrono 
 // function pegaArquivo (caminhoDoArquivo){
